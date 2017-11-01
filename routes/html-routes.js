79c0b7ac0f7c1a6b28ee4e7085a2527259730aaf
jtmPC <jon87tyler@gmail.com>
2017-11-01 15:32:43 -0700

    Built out initial api routes and html routes.  built super simple pages to test that the html routes work.  will need to update routes with final collumn names from the tables we create

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,4 +2,17 @@ var path = require("path");
 
 module.exports = function(app) {
 
+	app.get("/team-display", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/team.html"));
+	});
+
+	app.get("/enter-bout-stats", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/enterStats.html"));
+	});
+
+	app.get("*", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/index.html"));
+	});
+
 };
+
--- routes/team-api-routes.js
+++ routes/team-api-routes.js
@@ -1,5 +1,26 @@
 var db = require("../models");
 
+var Team = require("../models/team.js");
+
 module.exports = function(app) {
 
+	// Route to get all teams
+	app.get("/api/team", function (req, res) {
+		Team.findAll({}).then(function (dbTeam){
+			res.json(dbTeam);
+		});
+	});
+
+	// Route to get a specific team
+	app.get("/api/team/:teamName", function (req, res) {
+		Team.findOne({
+			where: {
+				teamName: req.params.teamName
+			}
+		}).then(function(data) {
+			res.json(data);
+		});
+	});
+
 };
+
